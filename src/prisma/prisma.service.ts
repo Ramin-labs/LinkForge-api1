e3c3a8494845e713f022c8a8d9962e435493433c
Ramin Khalili <ramin.khalili@studenti.unipd.it>
2025-11-06 11:07:30 +0100

    chore: skip database connection initialization during test runs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,11 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    // Avoid connecting to the database during test runs.
+    // Prisma will lazily establish a connection on the first query anyway.
+    if (process.env.NODE_ENV === 'test') {
+      return;
+    }
     await this.$connect();
   }
   async enableShutdownHooks(app: INestApplication) {
